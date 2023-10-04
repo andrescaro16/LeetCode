998f672e821c0cc75a813b12c7713fad8ef9e397
andrescaro16 <julicaro2003@gmail.com>
2023-10-04 18:58:37 -0500

    Add gitignore and Merge two lists problem

---
+++ Problems/003_Merge-Two-Lists.py
@@ -0,0 +1,34 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def mergeTwoLists(self, list1, list2):
+        mergedList = ListNode()
+        current = mergedList
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        current.next = list1 or list2
+
+        return mergedList.next
+
+        
+        
+
+if __name__ == '__main__':
+    list1 = ListNode(2)
+    list2 = ListNode(3)
+
+    solution = Solution()
+    print(solution.mergeTwoLists(list1, list2).val)
