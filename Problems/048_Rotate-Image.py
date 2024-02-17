91c3cbfab6420ea435e37e8f5d637fe7acf4f7a2
andrescaro16 <julicaro2003@gmail.com>
2024-02-17 17:24:31 -0500

    Improve 48 problem solution

--- Problems/048_Rotate-Image.py
+++ Problems/048_Rotate-Image.py
@@ -1,17 +1,38 @@
 class Solution:
+    
+    # In-place
+    # Time complexity: O(n^2)
+    # Space complexity: O(1)
+    def rotate(self, matrix):
+        
+        n = len(matrix)
+        
+        # Transposing matrix
+        for i in range(0, n-1):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # Reversing matrix
+        for i in matrix:
+            i.reverse()
+        
+        return matrix
+
+
+    # Out-place
     # Time complexity: O(n^2)
     # Space complexity: O(n^2)
-    def rotate(self, matrix):
-        rotatedMatrix = [[0 for i in range(len(matrix))] for i in range(len(matrix))]
-        print(rotatedMatrix)
+    # def rotate(self, matrix):
+    #     rotatedMatrix = [[0 for i in range(len(matrix))] for i in range(len(matrix))]
+    #     print(rotatedMatrix)
         
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                print(rotatedMatrix)
+    #     for i in range(len(matrix)):
+    #         for j in range(len(matrix)):
+    #             print(rotatedMatrix)
 
-                rotatedMatrix[j][len(matrix) - i - 1] = matrix[i][j]
+    #             rotatedMatrix[j][len(matrix) - i - 1] = matrix[i][j]
 
-        return rotatedMatrix
+    #     return rotatedMatrix
 
 
 if __name__ == "__main__":
