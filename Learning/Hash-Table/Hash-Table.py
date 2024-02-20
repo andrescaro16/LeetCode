d719e24fabda9f2784ec848efd56822609ab0ac1
andrescaro16 <julicaro2003@gmail.com>
2024-02-20 09:49:52 -0500

    Improve hash table implementation with chaining
    Chaining allow us to handle the collisions on hash tables

--- Learning/Hash-Table/Hash-Table.py
+++ Learning/Hash-Table/Hash-Table.py
@@ -1,8 +1,8 @@
 class HashTable:
     
     def __init__(self):
-        self.max = 1000
-        self.arr = [None for _ in range(self.max)]
+        self.max = 10
+        self.arr = [[] for _ in range(self.max)]
     
     def getHash(self, key):
         sum = 0
@@ -12,20 +12,45 @@ class HashTable:
 
     def __setitem__(self, key, value):
         hash = self.getHash(key)
-        self.arr[hash] = value
+        
+        found = False
+        for i, pair in enumerate(self.arr[hash]):
+            if len(pair) == 2 and pair[0] == key:
+                self.arr[hash][i] = (key, value)
+                found = True
+                break
+        
+        if not found:
+            self.arr[hash].append((key, value))
     
     def __getitem__(self, key):
         hash = self.getHash(key)
-        return self.arr[hash]
-    
+        for pair in self.arr[hash]:
+            if len(pair) == 2 and pair[0] == key:
+                return pair[1]
+
     def __delitem__(self, key):
         hash = self.getHash(key)
-        self.arr[hash] = None
+        for i, pair in enumerate(self.arr[hash]):
+            if pair[0] == key:
+                del self.arr[hash][i]
     
 
 if __name__ == "__main__":
+    
     dictionary = HashTable()
+    
     dictionary["January/16/2023"] = 86500
+    dictionary["January/16/2023"] = 100000
+    dictionary["January/17/2023"] = 90000
+    dictionary["January/17/2023"] = 80000
+    dictionary["January/18/1023"] = 50000
+
+    print(dictionary.arr)
     print(dictionary["January/16/2023"])
-    del dictionary["January/16/2023"]
-    print(dictionary["January/16/2023"])
+    print(dictionary["January/17/2023"])
+    print(dictionary["January/18/1023"])
+    print(dictionary["Whatever"])
+    
+    del dictionary["January/18/1023"]
+    print(dictionary["January/18/1023"])    # -> None
